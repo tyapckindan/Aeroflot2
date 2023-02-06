1c93d8fa383ea10382b1b14f8f4765af451a155c
Данила Тяпкин <dtapkin6@gmail.com>
2023-02-06 10:51:46 +0300

    Добавьте файлы проекта.

---
+++ Aeroflot2/Aeroflot.cs
@@ -0,0 +1,41 @@
+﻿class Aeroflot
+{
+    string point, day, type, number;
+    public int time;
+    public string getDay
+    {
+        get
+        {
+            return day;
+        }
+    }
+
+    public string getPoint
+    {
+        get
+        {
+            return point;
+        }
+    }
+
+    public int getTime
+    {
+        get
+        {
+            return time;
+        }
+    }
+    public Aeroflot(string point, string day, string type, string number, int time)
+    {
+         this.point = point;
+         this.day = day;
+         this.type = type;
+         this.number = number;
+         this.time = time;
+    }
+    public string GetInfo()
+    {
+        return ("Тип самолёта: " + type + ", Пункт назначения: " + point + ", " +
+            "Номер рейса: " + number + ", День: " + day + "."+ "\nВремя отправления: " + time + " часов.");
+    }
+}
---
+++ Aeroflot2/Program.cs
@@ -0,0 +1,55 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        List<Aeroflot> planes = new List<Aeroflot>();
+        planes.Add(new Aeroflot("Лондон", "Понедельник", "Boening-1", "1070", 17));
+        planes.Add(new Aeroflot("Москва", "Вторник", "Boening-2", "1071", 12));
+        Console.Write("Введите день недели: ");
+        string request = Console.ReadLine();
+        string response = "";
+        foreach (Aeroflot plane in planes)
+        {
+            if (plane.getDay == request)
+            {
+                response = plane.GetInfo();
+                break;
+            }
+        }
+        if (response != "")
+            Console.WriteLine(response);
+        else Console.WriteLine("Рейсы на данный день недели отсутствуют.");
+        response = "";
+
+        Console.Write("Введите пункт назначения: ");
+        request = Console.ReadLine();
+        foreach (Aeroflot plane in planes)
+        {
+            if (plane.getPoint == request)
+            {
+                response = plane.GetInfo();
+                break;
+            }
+        }
+        if (response != "")
+            Console.WriteLine(response);
+        else Console.WriteLine("Рейсы на данный пункт назначения отсутствуют.");
+        response = "";
+
+        Console.Write("Введите день недели: ");
+        request = Console.ReadLine();
+        Console.Write("Введите время вылета: ");
+        int time = int.Parse(Console.ReadLine());
+        foreach (Aeroflot plane in planes)
+        {
+            if (plane.getDay == request && plane.time > time)
+            {
+                response = plane.GetInfo();
+                break;
+            }
+        }
+        if (response != "")
+            Console.WriteLine(response);
+        else Console.WriteLine("Рейсы на данный день недели, время вылета которых больше заданного отсутствуют.");
+    }
+}
